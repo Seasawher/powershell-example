236bda37cfd38f599b7b71e1a1019bf8f69dcc0f
seasawher <tellagoodstory@icloud.com>
2023-07-13 22:56:51 +0900

    Gitリポジトリかどうか判定するスクリプトを実装

---
+++ module/DetectGit.ps1
@@ -0,0 +1,35 @@
+function DetectGit {
+    <#
+    .SYNOPSIS
+    Gitリポジトリかどうか判定する
+    #>
+    Param (
+        [String]
+        # 対象となるディレクトリ
+        $workdir
+    )
+    Set-Location -Path $workdir
+
+    $gitPath = Join-Path $workdir ".git"
+    $gitExists = Test-Path -Path $gitPath
+
+    Set-Location -Path $PSScriptRoot
+    return $gitExists
+}
+
+describe 'DetectGit' {
+    BeforeAll {
+        $testPath = "TestDrive:\test.txt"
+        Set-Content $testPath -value "my test text."
+    }
+
+    it "Gitリポジトリでなければ False を返す" {
+        DetectGit -workdir $TestDrive | Should Be $false
+    }
+
+    it "Gitリポジトリなら True を返す" {
+        Set-Location -Path $TestDrive
+        git init
+        DetectGit -workdir $TestDrive | Should Be $true
+    }
+}
