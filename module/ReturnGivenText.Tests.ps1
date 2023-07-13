c93d88a0eb3f79edf31569ce3d2fe2d03dd0c3ca
seasawher <tellagoodstory@icloud.com>
2023-07-13 22:30:43 +0900

    テストコードを独立させない

--- module/ReturnGivenText.Tests.ps1
+++
@@ -1,9 +0,0 @@
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.ps1', '.psm1'
-Import-Module "$here\$sut" -Force
-
-describe 'ReturnGivenText' {
-    it "入力として HelloWorld が与えられたら，それを返す" {
-        ReturnGivenText -text "HelloWorld" | Should Be "HelloWorld"
-    }
-}
